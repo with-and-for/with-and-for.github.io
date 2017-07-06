fc82c356945fd783adc829ed28fccec31febc6cc
tzarras <TimKitchen@Tims-MacBook-Pro.local>
2017-07-06 15:12:03 -0400

    yo

--- ngd/ngd.js
+++ ngd/ngd.js
@@ -22,7 +22,6 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
 
       for (var i = 0; i < results.length; i++) {
 
-        image = results[i].getImage("article_and_recipe.image").asHtml();
         number = results[i].getNumber("article_and_recipe.number");
         date = results[i].getStructuredText("article_and_recipe.date").asText();
         title = results[i].getStructuredText("article_and_recipe.article_title").asText();
@@ -31,24 +30,21 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
        	recipe = results[i].getStructuredText("article_and_recipe.recipe").asHtml();
         keyword = results[i].getStructuredText("article_and_recipe.keywords").asHtml();
      	
-      var imagezone = $("<div class='imagezone'></div>")
      	var card = $("<div class='card'></div>");
       var dateP = $("<h1 class='date'></h1>");
      	var titleP = $("<h1 class='title'></h1>");
      	var articleP = $("<p class='article'></p>");
      	var numberP = $("<p class='number' style='display:none'></p>");
      	var recipearea = $("<div class='recipearea'></div>");
-     	var recipecardarea = $("<div class='recipecardarea'></div>");
      	var recipetitleP = $("<h1 class='recipetitle'></h1>");
      	var recipeP = $("<p class='recipe'></p>");
-      var print = $("<p class='print'>Print this recipe</p>");
-      var Read = $("<p class='read'>Read more</p>");
+      var print = $("<p class='print'>print this recipe</p>");
+      var Read = $("<p class='read'>read more</p>");
       var keywords = $("<div class='keywords'></div>");
       var word = $("<p class='keyword'></p>");
 
       word.append(keyword);
       keywords.append(word);
-      imagezone.append(image);
       dateP.append(date);
      	titleP.append(title);
      	articleP.append(article);
@@ -56,8 +52,8 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
      	recipeP.append(recipe);
      	numberP.append(number);
      	recipearea.append(numberP,recipetitleP,recipeP,print);
-      card.append(numberP,keywords,titleP,dateP,imagezone,articleP,recipearea,Read);
-      body.append(card,recipecardarea);
+      card.append(numberP,keywords,titleP,dateP,articleP,recipearea,Read);
+      body.append(card);
 
 
       }
@@ -65,7 +61,6 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
 
       var $divs = $("div.card");
 
-      // sort projects by hidden number value, largest to smallest //
 
       var ordered = $divs.sort(function (a, b) {
 
@@ -73,13 +68,8 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
 
       });
 
-      // append reordered divs to project section //
-
       $(ordered).appendTo("body");
 
-      $(".recipearea").clone().addClass("just").appendTo(".recipecardarea");
-
-      $(".card > .article").hide();
 
 
       if( /Android|webOS|iPhone|iPod|Opera Mini/i.test(navigator.userAgent) ) {
@@ -88,11 +78,10 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
 
 
         $("body").on("click",".card", function(){
-        var height = $(".article",this).height()+$(".recipearea",this).height()+$(".imagezone > img",this).height()+800;
+        var height = $(".article",this).height()+$(".recipearea",this).height()+450;
         console.log(height);
         $(this).css({"height":height,"width":"100vw"});
         $(".article",this).not(".number").show().css("positon","relative");
-        $(".imagezone > img",this).css({"positon":"relative","left":"1vw","width":"96vw","height":"auto","top":"6vh","display":"inline"}).show();
         $(".read",this).hide();
         $(".recipearea",this).show();
         $(".keywords",this).hide();
@@ -132,11 +121,11 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
         });
 
         $("body").on("click",".card", function(){
-        var height = $(".article",this).height()+$(".recipearea",this).height()+400;
+        var height = $(".article",this).height()+$(".recipearea",this).height();
+        var padding = $(window).height()*.5;
         console.log(height);
-        $(this).css({"height":height,"width":"80vw"});
+        $(this).css({"height":height+padding});
         $(".article",this).not(".number").show();
-        $(".imagezone > img",this).css({"right":"0vw","width":"30%","height":"auto","top":"22vh"}).show();
         $(".read",this).hide();
         $(".recipearea",this).show();
         $(".keywords",this).hide();
@@ -188,12 +177,6 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
 
 
 
-
-
-
-
-
-
 // functions for mostly static elements //
 
 
@@ -205,65 +188,38 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
 
   if( /Android|webOS|iPhone|iPod|Opera Mini/i.test(navigator.userAgent) ) {
 
-    $(window).scroll(function(){
+  $(window).scroll(function(){
       var height = $(window).height();
-      var top = $(window).height()/4;
-
 
     if ($(window).scrollTop() > $(window).height()/4){
       $(".lime,.spinach,.strawberry,.banana,.blueberry").css({"-webkit-filter":"blur(5px)","opacity":".1"});
-
     } else {
       $(".lime,.spinach,.strawberry,.banana,.blueberry").css({"-webkit-filter":"blur(0px)","opacity":"1"});
-
     }
 
-    if ($(window).scrollTop() > height - top){
-      $(".down").css({"position":"fixed","top":"-10vh","z-index":"999","left":"30vw","font-size":"14"});
-      $(".search").css({"position":"fixed","top":"10vh"});
-      $(".only").css({"position":"fixed","top":"16vh"});
+    if ($(window).scrollTop() > height -20){
+      $(".topbar").css({"position":"fixed","top":"0vh"});
     } else {
-      $(".down").css({"position":"absolute","left":"28vw","top":"80vh","font-size":"20"});
-      $(".search").css({"position":"absolute","top":"110vh"});
-      $(".only").css({"position":"absolute","top":"118vh"});
+      $(".topbar").css({"position":"absolute","top":"100vh"});
     }
 
   });
 
 
-  
-    ///
-
-    $(".down").click(function(){
+  $(".down,.navtitle").click(function(){
       var blog = $(window).height();
-      var per = $(window).height()*.0009;
-
-      $("html,body").animate({scrollTop: blog-per},700);
+      $("html,body").animate({scrollTop: blog},700);
       $(".card").css("height","150px");
-      $(".imagezone > img").hide();
       $(".article,.recipearea").hide();
       $(".read").show();
   
-    });
-
-
-
+  });
 
 
 
 // not phone //
 
-
-
-
-
-
   } else {
-
-
-
-
-
 
 
     $(window).scroll(function(){
@@ -272,64 +228,42 @@ Prismic.Api('https://nicholas-dutton.prismic.io/api', function (err, Api) {
 
 
     if ($(window).scrollTop() > $(window).height()/4){
-      $(".lime,.spinach,.strawberry,.banana,.blueberry").css({"-webkit-filter":"blur(5px)","filter":"blur(5px)","opacity":"1"});
-      
-
-
+      $(".lime,.spinach,.strawberry,.banana,.blueberry").css({"-webkit-filter":"blur(5px)","filter":"blur(5px)","opacity":".1"});
     } else {
       $(".lime,.spinach,.strawberry,.banana,.blueberry").css({"-webkit-filter":"blur(0px)","filter":"blur(0px)","opacity":"1"});
      
     }
 
-    if ($(window).scrollTop() > height - top){
-      $(".down").css({"position":"fixed","top":"3vh","z-index":"999","left":".5vw","font-size":"14"});
-      $(".search").css({"position":"fixed","top":"10vh"});
-      $(".only").css({"position":"fixed","top":"16vh"});
+    if ($(window).scrollTop() > height - 100){
+      $(".topbar").css({"position":"fixed","top":"0vh"});
     } else {
-      $(".down").css({"position":"absolute","left":"4.25vw","top":"80vh","font-size":"38"});
-      $(".search").css({"position":"absolute","top":"110vh"});
-      $(".only").css({"position":"absolute","top":"118vh"});
+      $(".topbar").css({"position":"absolute","top":"100vh"});
     }
 
   });
 
-	
-
-	$(".only").click(function(){
-		var blog = $(window).height();
-    var per = $(window).height()/20;
-    var par = $(window).height()*.0009;
-		$("html,body").animate({scrollTop: blog-per},200);
-		$(".recipecardarea").toggle();
-		$(".card").toggle();
-		$(".only").toggleClass("clicked");
-		if ($(".only").text() == "recipes only!") {
-            $(".only").html("<p>back to blog</p>");
-
-      
-        }
-        else {
-            $(".only").html("<p>recipes only!</p>");
-            $(".down").css("pointer-events","all");
-            $("html,body").animate({scrollTop: blog-par},200);
-            $(".card").css("height","150px");
-            $(".article").hide();
-            $(".read").show();
-            $(".imagezone > img").css({"right":"6vw","width":"auto","height":"80%","top":"10%"});
-        }
-	});
-
    // scroll to blog //
 
-    $(".down").click(function(){
+    $(".down,.navtitle").click(function(){
       var blog = $(window).height();
-      var per = $(window).height()*.0009;
 
       //$(".only").html("<p>recipes only!</p>").removeClass("clicked");
-      $("html,body").animate({scrollTop: blog-per},700);
+      $("html,body").animate({scrollTop: blog},700);
       $(".recipecardarea").hide();
       $(".card").css({"height":"150px","width":"80vw"}).show();
-      $(".article,.recipearea,.imagezone > img").hide();
+      $(".article,.recipearea").hide();
+      $(".read,.keywords").show();
+  
+    });
+
+    $(".name").click(function(){
+      var blog = $(window).height();
+
+      //$(".only").html("<p>recipes only!</p>").removeClass("clicked");
+      $("html,body").animate({scrollTop: 0},700);
+      $(".recipecardarea").hide();
+      $(".card").css({"height":"150px","width":"80vw"}).show();
+      $(".article,.recipearea").hide();
       $(".read,.keywords").show();
   
     });
