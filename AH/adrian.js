052140aac4b89c0f5222d77fbf2b594ed0540dca
tzarras <TimKitchen@Tims-MacBook-Pro.local>
2017-10-05 22:00:36 -0400

    updates

--- AH/adrian.js
+++ AH/adrian.js
@@ -24,98 +24,105 @@ Prismic.Api('https://adrianhinojosa.prismic.io/api', function (err, Api) {
       	body.append(imageD);
 
           var beep = $(images).attr("src");
-          src.push(beep)
-          console.log(beep);
+          src.push(beep);
 
-
+          // $(images).css("top",100*i+"vh");
 
       }
 
+      $(".images").hide();
 
+      setTimeout(function(){
+        $(".images").fadeIn(400);
+      },500);
 
+if ($(window).width() > 736) {
 
-
-if ($(window).width() > 736){
-
-
-$(".images").hide();
-
-setTimeout(function(){
-  $(".images").fadeIn(500);
-},500);
-
+  // assign each div its corresponding source//
   $(".images").each(function(){
     var index = $(this).index(".images");
-    $(this).attr("yo",index).css({"background":"url("+src[index]+")","background-size": "auto 100vh","background-repeat":"repeat-x","background-position": "center","background-attachment": "fixed"});
+    $(this).css("background","url("+src[index]+")");
   });
-  var length = $(".images").length;
-  console.log(length);
-  var click = 0;
 
+  // sort divs highest to lowest//
   var $divs = $("div.images");
 
   var ordered = $divs.sort(function (a, b) {
-   return $(a).find(".order").text() - $(b).find(".order").text();
+   return $(b).find(".order").text() - $(a).find(".order").text();
   });
 
   $(ordered).appendTo("body");
 
+  // set background image properties //
+  $(".images").each(function(){
+    var order = $(this).find(".order").text();
+    var index = $(this).index(".images");
 
-      $(".images").click(function () {
-        click++;
-        if (click < length){
-          $(this).animate({"height":"0px"},700);
-        } else {
-          console.log("hello");
-          click = 0;
-          $(".images").animate({"top":"0","height":"100vh"});
-          $(".images:first").delay(400).fadeIn(200);
-          $(".images").delay(300).show();
-        }
+    console.log(index,length);
+      $(this).css({
+      "background-size": "auto 100vh",
+      "background-repeat":"repeat-x",
+      "background-position": "center",
+      "background-attachment": "fixed"});
+
+    });
+
+    // wait to offset each container div //
+
+    setTimeout(function(){
+      $(".images").each(function(){
+        var index = $(this).index(".images");
+        $(this).css({
+          "top":100*index+"vh"
+          });
       });
+    },50);
+
 
  } else {
 
- $(".images").hide();
 
- setTimeout(function(){
-   $(".images").fadeIn(500);
- },500);
+ // assign each div its corresponding source//
+ $(".images").each(function(){
+   var index = $(this).index(".images");
+   $(this).css("background","url("+src[index]+")");
+ });
 
+ // sort divs highest to lowest//
+ var $divs = $("div.images");
 
-   $(".images").each(function(){
-     var index = $(this).index(".images");
-     $(this).attr("yo",index).css({"background":"url("+src[index]+")","background-size": "100vw auto","background-repeat":"repeat-y"});
+ var ordered = $divs.sort(function (a, b) {
+  return $(b).find(".order").text() - $(a).find(".order").text();
+ });
+
+ $(ordered).appendTo("body");
+
+ // set background image properties //
+ $(".images").each(function(){
+   var order = $(this).find(".order").text();
+   var index = $(this).index(".images");
+
+   console.log(index,length);
+     $(this).css({
+     "background-size": "100vw auto",
+     "background-repeat":"repeat-y",
+     "background-position": "center",
+     "background-attachment": "fixed"});
+
    });
 
-   var $divs = $("div.images");
+   // wait to offset each container div //
 
-   var ordered = $divs.sort(function (a, b) {
-    return $(a).find(".order").text() - $(b).find(".order").text();
-   });
+   setTimeout(function(){
+     $(".images").each(function(){
+       var index = $(this).index(".images");
+       $(this).css({
+         "top":100*index+"vh"
+         });
+     });
+   },50);
 
-   $(ordered).appendTo("body");
-
-   var length = $(".images").length;
-   console.log(length);
-   var click = 0;
-
-   $(".images").click(function () {
-     click++;
-     if (click < length){
-       $(this).animate({"height":"0px"},700);
-     } else {
-       console.log("hello");
-       click = 0;
-       $(".images").animate({"top":"0","height":"100vh"});
-       $(".images:first").delay(400).fadeIn(200);
-       $(".images").delay(300).show();
-     }
-   });
-
-  }
-
-
+ }
 
     });
 
@@ -149,12 +156,10 @@ setTimeout(function(){
 				$(document).mousemove(function(){
 					var half = $(window).width()*.35;
 					if (event.pageX < half){
-            $("body").css("cursor","pointer");
-						$("h1,.contact > p > a").css({"color":"white","font-size":"18"});
+						$("h1,.contact > p > a").css({"color":"white"});
             $(".contact").fadeIn(400);
 					} else {
-            $("body").css("cursor","url(down.png) 10 20, auto");
-						$("h1,.contact > p > a").css({"color":"#a5a5a5","font-size":"14"});
+						$("h1,.contact > p > a").css({"color":"#a5a5a5"});
             $(".contact").hide();
 					}
 				});
